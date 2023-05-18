474f78d176bcae63f3ffeb77957c062296d8ff0a
George <penkov.gd@gmail.com>
2023-05-18 19:49:51 +0300

    max password len upd

--- frontend/app/src/helpers/validators.ts
+++ frontend/app/src/helpers/validators.ts
@@ -2,7 +2,7 @@ import * as zod from "zod";
 
 export const authSchema = zod.object({
     email: zod.string().email("Invalid email address"),
-    password: zod.string().min(8, "Must be 8 or more characters long").max(32, "Must be 32 or fewer characters long"),
+    password: zod.string().min(8, "Must be 8 or more characters long").max(100, "Must be 100 or fewer characters long"),
     name: zod.string().max(30, "Must be 30 or fewer characters long"),
     surname: zod.string().max(30, "Must be 30 or fewer characters long"),
 });
