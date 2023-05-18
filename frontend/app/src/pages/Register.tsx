71492e05a58beb197151fd57c86ac1d9251bad87
George <penkov.gd@gmail.com>
2023-05-18 16:27:03 +0300

    udp валидация пароля на фронте (на беке не нужна)

--- frontend/app/src/pages/Register.tsx
+++ frontend/app/src/pages/Register.tsx
@@ -13,7 +13,7 @@ import * as zod from "zod";
 
 const registerSchema = zod.object({
     email: zod.string().email("Invalid email address"),
-    password: zod.string().min(8, "Must be 8 or more characters long"),
+    password: zod.string().min(8, "Must be 8 or more characters long").max(32, "Must be 32 or fewer characters long"),
     name: zod.string().max(30, "Must be 30 or fewer characters long"),
     surname: zod.string().max(30, "Must be 30 or fewer characters long"),
 });
