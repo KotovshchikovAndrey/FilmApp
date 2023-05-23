208b1bdfa9015dece255884af918bd5cb9c29539
George <penkov.gd@gmail.com>
2023-05-23 14:33:23 +0300

    with credentials: true

--- frontend/app/src/http/index.ts
+++ frontend/app/src/http/index.ts
@@ -2,26 +2,26 @@ import axios from 'axios'
 import {API_URL} from "../core/config";
 
 export const axiosAuthInstance = axios.create({
-    withCredentials: false,
+    withCredentials: true,
     baseURL: API_URL,
 })
 
 axiosAuthInstance.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')})`
+    config.headers.Authorization = `Bearer ${localStorage.getItem('access_token')})`
     return config
 })
 
 export const axiosInstance = axios.create({
-    withCredentials: false,
+    withCredentials: true,
     baseURL: API_URL,
 })
 
 export const axiosRefreshInstance = axios.create({
-    withCredentials: false,
+    withCredentials: true,
     baseURL: API_URL,
 })
 
 axiosRefreshInstance.interceptors.request.use((config) => {
-    config.headers["old_access"] = `${localStorage.getItem('token')})`
+    config.headers['old_access'] = localStorage.getItem('access_token')
     return config
 })
