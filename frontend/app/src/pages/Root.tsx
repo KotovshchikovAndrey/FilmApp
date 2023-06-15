1beaef60d5117385f45d5efa7b3b20ea96affe71
George <penkov.gd@gmail.com>
2023-06-15 15:57:37 +0300

    scroll to top

--- frontend/app/src/pages/Root.tsx
+++ frontend/app/src/pages/Root.tsx
@@ -2,6 +2,7 @@ import {Container} from "@mui/material";
 import React from "react";
 import {Outlet} from "react-router-dom";
 import {WarnHeader} from "../components/shared/Header/WarnHeader";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function Root() {
     return (
@@ -10,6 +11,7 @@ export default function Root() {
             <Container sx={{mb: 10}}>
                 <Outlet/>
             </Container>
+          <ScrollToTop/>
         </React.Fragment>
     );
 }
